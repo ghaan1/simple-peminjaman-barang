5d54e8daef67a6a6aab11fce71780a6a69617e60
ariqluth <ttzluthfi@gmail.com>
2023-03-31 18:05:06 +0700

    route update

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,7 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
         Route::resource('menu-group', MenuGroupController::class);
         Route::resource('menu-item', MenuItemController::class);
     });
-    
+
     Route::group(['prefix' => 'role-and-permission'], function () {
         //role
         Route::resource('role', RoleController::class);
@@ -77,13 +77,13 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
 
     Route::prefix('master-table-management')->group(function () {
         Route::get('/data-barang', function () {
-            return view('master-table/data-barang/index');
+            return view('master-table.data-barang.index');
         });
         Route::get('/data-peminjaman', function () {
-            return view('master-table/data-peminjaman/index');
+            return view('master-table.data-peminjaman.index');
         });
         Route::get('/data-admin', function () {
-            return view('master-table/data-admin/index');
+            return view('master-table.data-admin.index');
         });
 
     });
