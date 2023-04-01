0b9d90ab10698ceb34f65ea257f6d94411306dd2
ghaan1 <gono0608@gmail.com>
2023-04-02 02:46:24 +0700

    ganti seeder user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,13 +17,13 @@ class UserSeeder extends Seeder
     public function run()
     {
         User::create([
-            'name' => "SuperAdmin",
+            'name' => "Admin Kelurahan",
             'email' => "superadmin@gmail.com",
             'password' => Hash::make('password'),
             'email_verified_at' => now(),
         ]);
         User::create([
-            'name' => "user",
+            'name' => "Admin RT",
             'email' => "user@gmail.com",
             'password' => Hash::make('password'),
             'email_verified_at' => now(),
